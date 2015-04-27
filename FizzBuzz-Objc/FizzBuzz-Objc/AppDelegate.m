a790be97c4f3914768871c7f251b532e964b2427
Thomas Bloss <runredfox@gmail.com>
2015-04-27 03:35:39 -0600

    code added

--- FizzBuzz-Objc/FizzBuzz-Objc/AppDelegate.m
+++ FizzBuzz-Objc/FizzBuzz-Objc/AppDelegate.m
@@ -13,9 +13,50 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
-    // Insert code here
-    
-    return YES;
-}
 
-@end
+        
+        // step 4
+        
+        for (int x = 1; x < 100; x++) {
+            if ([self isBuzzed:x] == true && [self isFizzed:x]== false) {
+                NSLog(@"Buzz");
+            } else if ([self isBuzzed:x] == false && [self isFizzed:x] == true) {
+                NSLog(@"Fizz");
+            } else if ([self isBuzzed:x] == true && [self isFizzed:x] == true) {
+                NSLog(@"FizzBuzz");
+            } else {
+                NSLog(@"%i",x);
+            }
+        }
+        
+        return YES;
+    }
+
+    
+    -(BOOL)isBuzzed:(int)aValue {
+        if (aValue % 3 == 0) {
+            //        NSLog(@"is true");
+            return true;
+        }
+        else {
+            //      NSLog(@"is false");
+            return false;
+        }
+    }
+    
+
+    
+    -(BOOL)isFizzed:(int)aValue {
+        if (aValue % 5 == 0) {
+            //        NSLog(@"is true");
+            return true;
+        }
+        else {
+            //      NSLog(@"is false");
+            return false;
+        }
+    }
+    
+    
+    
+    @end
